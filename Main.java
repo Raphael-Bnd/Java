aeecd53a545da83235711c8218f0610c8127383c
Raphael Bandeira <1contatoraphael@gmail.com>
2025-02-10 23:47:48 -0300

    add print and variables

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Olá mundo!");
+        int idade = 25;
+        double altura = 1.75;
+        char sexo = 'F';
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(sexo);
     }
 }
